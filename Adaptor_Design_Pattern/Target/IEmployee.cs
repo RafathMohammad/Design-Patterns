1181e1d9f459fc8394bffa64e63d4b13d4c18ac8
rm185527 <rm185527@WINRM185527-I4Q>
2022-11-18 20:10:27 +0530

    Adaptor pattern changes

--- Adaptor_Design_Pattern/Target/IEmployee.cs
+++ Adaptor_Design_Pattern/Target/IEmployee.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Adaptor_Design_Pattern
+﻿namespace Adaptor_Design_Pattern
 {
     public interface IEmployee
     {
